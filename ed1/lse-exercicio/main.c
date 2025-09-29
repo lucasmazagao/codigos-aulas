1c60e7471967fe9b64ef415699c45d14f9f39879
lucasmazagao <lucas_mazagao@hotmail.com>
2025-09-28 22:32:37 -0300

    versao melhorada main.c

--- ed1/lse-exercicio/main.c
+++ ed1/lse-exercicio/main.c
@@ -6,31 +6,43 @@
 int main() {
     LSE *lista = lse_cria();
     if (!lista) {
-        return 1; // Erro na criação da lista
+        return 1; // Erro em criar lista
     }
     
     int opcao;
-    while (scanf("%d", &opcao) == 1) {
+    while (1) {
+        scanf("%d", &opcao);
+
         switch (opcao) {
             case 1: { // Inserir x no final
                 int x;
                 scanf("%d", &x);
                 int resultado = lse_insere_final(lista, x);
-                printf("%d\n", (resultado == 1) ? 1 : 0);
+                if (resultado == 1) { // sucesso
+                    printf("1\n");
+                } else { // falha
+                    printf("0\n");
+                }
                 break;
             }
             case 2: { // Inserir x na posição p
                 int p, x;
                 scanf("%d %d", &p, &x);
                 int resultado = lse_insere_pos(lista, p, x);
-                printf("%d\n", (resultado == 1) ? 1 : 0);
+
+                if (resultado == 1) {
+                    printf("1\n");
+                } else {
+                    printf("0\n");
+                }
                 break;
             }
-            case 3: { // Remover elemento da posição p
+            case 3: { // remove elemento da posição p
                 int p;
                 scanf("%d", &p);
                 LSE_Tipo removido;
                 int resultado = lse_remove_pos(lista, p, &removido);
+
                 if (resultado == 1) {
                     printf("%d\n", removido);
                 } else {
@@ -43,6 +55,7 @@ int main() {
                 scanf("%d", &p);
                 LSE_Tipo elem;
                 int resultado = lse_consulta_pos(lista, p, &elem);
+
                 if (resultado == 1) {
                     printf("%d\n", elem);
                 } else {
@@ -50,11 +63,12 @@ int main() {
                 }
                 break;
             }
-            case 5: { // Buscar primeira ocorrência de x
+            case 5: { // buscar primeira vez que x aparece
                 int x;
                 scanf("%d", &x);
                 int pos;
                 int resultado = lse_busca(lista, x, &pos);
+
                 if (resultado == 1) {
                     printf("%d\n", pos);
                 } else {
